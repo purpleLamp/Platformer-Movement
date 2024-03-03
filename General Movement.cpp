98831c69994a15f3672dde8858fc04bd99b57046
purpleLamp <107326394+purpleLamp@users.noreply.github.com>
2024-03-03 15:47:47 -0600

    Update General Movement.cpp
    Updated wall jumping 
    Implemented Dash

--- General Movement.cpp
+++ General Movement.cpp
@@ -12,10 +12,13 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     public LayerMask ground;
     bool facingRight = true;
+    public SpriteRenderer spriteRenderer;
+    private Color normalColor;
 
     //Movment
     public float moveSpeed;
     private float input;
+    private float yinput;
 
     //Jump
     bool jumpRequest;
@@ -25,25 +28,42 @@ public class PlayerMovement : MonoBehaviour
     public float fallGravity;
     public float normalGravity;
 
+    //Wall Sliding 
     private bool isWallSliding;
     public float wallSlidingSpeed = 2f;
 
+    //Wall Jumping
     private bool isWallJumping;
     private float wallJumpingTimer;
-    public Vector2 wallJumpingPower;
+    public Vector2 wallJumpingSpeed;
+    bool specialFuckingCircumstance = false;
+
+    //Dash
+    public bool canDash = true;
+    private bool isDashing;
+    public float dashingPower;
+    public float dashingTime;
+    public float dashingCooldown;
+    public TrailRenderer tr;
 
     private void OnDrawGizmos()
     {
-        /**
-        Debug.DrawRay(new Vector2(transform.position.x - 0.5f, transform.position.y - 1), Vector2.right, UnityEngine.Color.red);
+        
+        Debug.DrawRay(new Vector2(transform.position.x - 0.35f, transform.position.y - 1), new Vector2(0.7f,0), UnityEngine.Color.red);
 
-        if (facingRight) Debug.DrawRay(new Vector2(transform.position.x + 0.7f, transform.position.y - 0.7f), new Vector2(0, 1.5f), UnityEngine.Color.red);
-        else Debug.DrawRay(new Vector2(transform.position.x - 0.7f, transform.position.y - 0.7f), new Vector2(0,1.5f), UnityEngine.Color.red);
-        **/
+        if (facingRight) Debug.DrawRay(new Vector2(transform.position.x + 0.7f, transform.position.y - 0.7f), new Vector2(0, 1.3f), UnityEngine.Color.red);
+        else Debug.DrawRay(new Vector2(transform.position.x - 0.7f, transform.position.y - 0.7f), new Vector2(0,1.3f), UnityEngine.Color.red);
+        
+    }
+    private void Start()
+    {
+        normalColor = spriteRenderer.color;
     }
 
     void Update()
     {
+        if (isDashing) return;
+        
         if (!isWallJumping)
         {
             rb.velocity = new Vector2(input * moveSpeed, rb.velocity.y);
@@ -67,10 +87,13 @@ public class PlayerMovement : MonoBehaviour
             rb.gravityScale = normalGravity;
         }
         wallStuff();
+
+        //Mathf.Clamp(rb.velocity.y, -20, 20);
     }
     public void OnMove(InputAction.CallbackContext context)
     {
         input = context.ReadValue<Vector2>().x;
+        yinput = context.ReadValue<Vector2>().y;
 
         if (input < 0) facingRight = false;
         else if (input > 0) facingRight = true;
@@ -89,29 +112,63 @@ public class PlayerMovement : MonoBehaviour
         {
             jumpRequest = false;
             isWallJumping = false;
+            specialFuckingCircumstance = false;
         }
     }
 
     private void FixedUpdate()
     {
-        if (jumpRequest && !wallCheck())
+        if (isDashing) return;
+
+        if (jumpRequest && !wallCheck() && !specialFuckingCircumstance)
         {
             rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
             jumpTimer += Time.deltaTime;
-        }       
+        }
+
+        if (jumpRequest && specialFuckingCircumstance)
+        {
+            rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            jumpTimer += Time.deltaTime;
+        }
     }
     private bool isGrounded()
     {
-        return Physics2D.Raycast(new Vector2(transform.position.x - 0.5f, transform.position.y - 1), Vector2.right, 1.0f, ground);
+        return Physics2D.Raycast(new Vector2(transform.position.x - 0.35f, transform.position.y - 1), Vector2.right, 0.7f, ground);
     }
     private bool wallCheck()
     {
-        if (facingRight) return Physics2D.Raycast(new Vector2(transform.position.x + 0.7f, transform.position.y - 0.7f), Vector2.up, 1.5f, ground);
-        else return Physics2D.Raycast(new Vector2(transform.position.x - 0.7f, transform.position.y - 0.7f), Vector2.up, 1.5f, ground);
+        if (facingRight) return Physics2D.Raycast(new Vector2(transform.position.x + 0.7f, transform.position.y - 0.7f), Vector2.up, 1.3f, ground);
+        else return Physics2D.Raycast(new Vector2(transform.position.x - 0.7f, transform.position.y - 0.7f), Vector2.up, 1.3f, ground);
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(input * dashingPower, yinput * dashingPower);
+        tr.emitting = true;
+        spriteRenderer.color = Color.white;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+     
+    }
+
+    public void DashHandler(InputAction.CallbackContext context)
+    {
+        if (context.performed && canDash && (input != 0 || yinput != 0))
+        {
+            StartCoroutine(Dash());
+        }
     }
 
     private void wallStuff()
     {
+        //Wall Sliding
         if (wallCheck() && input != 0f)
         {
             isWallSliding = true;
@@ -122,25 +179,37 @@ public class PlayerMovement : MonoBehaviour
             isWallSliding = false;
         }
 
-        if (jumpRequest && isWallSliding && !isGrounded())
+        //WallJumping
+        if (jumpRequest && isWallSliding && !isGrounded() && !specialFuckingCircumstance)
         {
             if (!facingRight)
             {
                 isWallJumping = true;
-                rb.velocity = new Vector2(wallJumpingPower.x, wallJumpingPower.y);
+                rb.velocity = new Vector2(wallJumpingSpeed.x, wallJumpingSpeed.y);
                 jumpTimer += Time.deltaTime;
             }
             else
             {
                 isWallJumping = true;
-                rb.velocity = new Vector2(-wallJumpingPower.x, wallJumpingPower.y);
+                rb.velocity = new Vector2(-wallJumpingSpeed.x, wallJumpingSpeed.y);
                 jumpTimer += Time.deltaTime;
             }
         }
 
+        //Other Checks
         if (isGrounded())
         {
             isWallJumping = false;
+            canDash = true;
+            spriteRenderer.color = normalColor;
+        }
+        if (isGrounded() && wallCheck())
+        {
+            specialFuckingCircumstance = true;
+        }
+        if (!wallCheck() || !jumpRequest || isWallJumping)
+        {
+            specialFuckingCircumstance = false;
         }
     }
 
